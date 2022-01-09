56da27dd692b95dbbdc089ec856e8b5ea608fc69
yeonlisa <tkzw4869@gmail.com>
2022-01-09 17:14:20 +0900

    Favorite list에 추가 삭제

--- client/src/components/views/MovieDetail/Sections/Favorite.js
+++ client/src/components/views/MovieDetail/Sections/Favorite.js
@@ -1,36 +1,41 @@
 /*eslint-disable*/
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import Axios from 'axios';
+import { Button } from 'antd';
+
 
 function Favorite(props) {
-
+    
     const movieId = props.movieId;
     const userFrom = props.userFrom;
     const movieTitle = props.movieInfo.title;
     const moviePost = props.movieInfo.backdrop_path;
     const movieRuntime = props.movieInfo.runtime;
-
+    
     const [FavoriteNumber, setFavoriteNumber] = useState(0);
     const [Favorited, setFavorited] = useState(false);
+    let variables = {
+        userFrom : userFrom,
+        movieId: movieId,
+        movieTitle : movieTitle,
+        moviePost : moviePost,
+        movieRuntime: movieRuntime
+    }
 
     useEffect(() => {
 
-        let variables = {
-            userFrom,
-            movieId
-        }
 
-        axios.post('/api/favorite/favoriteNumber', variables)
+        Axios.post('/api/favorite/favoriteNumber', variables)
             .then(response => {
+                setFavoriteNumber(response.data.favoriteNumber)
                 if(response.data.success) {
-                    setFavoriteNumber(response.data.favoriteNumber)
                 } else {
                     alert('숫자 정보를 가져오는데 실패 했습니다.');
                 }
             })
 
 
-        axios.post('/api/favorite/favorited', variables)
+        Axios.post('/api/favorite/favorited', variables)
             .then(response => {
                 if(response.data.success) {
                     setFavorited(response.data.favorited)
@@ -41,9 +46,35 @@ function Favorite(props) {
 
     }, [])
 
+    const onClickFavorite = () => {
+        if(Favorited) {
+            Axios.post('/api/favorite/removeFromFavorite', variables)
+                .then(response => {
+                    if(response.data.success) {
+                        setFavoriteNumber(FavoriteNumber - 1)
+                        setFavorited(!Favorited)
+                    } else {
+                        alert('Favorite 리스트에서 지우는 것을 실패했습니다.')
+                    }
+                })
+
+        } else {
+            Axios.post('/api/favorite/addToFavorite', variables)
+                .then(response => {
+                    if(response.data.success) {
+                        setFavoriteNumber(FavoriteNumber + 1)
+                        setFavorited(!Favorited)
+                    } else {
+                        alert('Favorite 리스트에서 추가하는 것을 실패했습니다.')
+                    }
+                })
+
+        }
+    }
+
     return (
         <div>
-            <button>{Favorited ? "Not Favorite" : "Add to Favorite"} {FavoriteNumber}</button>
+            <Button onClick={onClickFavorite}>{Favorited ? "Not Favorite" : "Add to Favorite"} {FavoriteNumber}</Button>
         </div>
     )
 }
--- server/routes/favorite.js
+++ server/routes/favorite.js
@@ -17,8 +17,6 @@ router.post('/favoriteNumber', (req, res) => {
 router.post('/favorited', (req, res) => {
 
     // 내가 이 영화를 Favorite 리스트에 넣었는지 정보를 DB에서 가져오기
-
-    // mongoDB에서 favorite 숫자를 가져오기
     Favorite.find({ "movieId" : req.body.movieId , "userFrom" : req.body.userFrom })
         .exec((err, info) => {
             if(err) return res.status(400).send(err)
@@ -30,6 +28,27 @@ router.post('/favorited', (req, res) => {
 
             res.status(200).json({ success: true , favorited: result })
         })
+
+})
+
+router.post('/removeFromFavorite', (req, res) => {
+
+    Favorite.findOneAndDelete({ movieId : req.body.movieId , userFrom : req.body.userFrom })
+        .exec((err, doc) => {
+            if(err) return res.status(400).send(err)
+            res.status(200).json({ success: true, doc })
+        })
+
+})
+
+router.post('/addToFavorite', (req, res) => {
+
+    const favorite = new Favorite(req.body)
+
+    favorite.save((err, doc) => {
+        if(err) return res.status(400).send(err)
+        return res.status(200).json({ success: true })
+    })
 
 })
 
